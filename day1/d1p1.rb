bef950e9ac62503dc638717c2f2c2b0bcaac1aeb
Kyle Hanish <kyle.hanish@gmail.com>
2021-12-03 19:22:39 -0600

    move day1 to folder

---
+++ day1/d1p1.rb
@@ -0,0 +1,7 @@
+require '../input.rb'
+
+@filename = 'd1-input.txt'
+
+lines =  Input.get_file_lines(@filename).compact.map(&:to_i)
+result = lines.each_cons(2).select { |a,b| a < b }.length
+puts result
