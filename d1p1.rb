554789f03a935f9a68eec2ce99eca6dfb751f668
Kyle Hanish <kyle.hanish@gmail.com>
2021-12-03 19:16:45 -0600

    simplify

--- d1p1.rb
+++ d1p1.rb
@@ -3,5 +3,5 @@ require './input.rb'
 @filename = 'd1-input.txt'
 
 lines =  Input.get_file_lines(@filename).compact.map(&:to_i)
-result = lines.each_cons(2).map { |a,b| a < b ? 1 : 0 }.reduce(:+)
+result = lines.each_cons(2).select { |a,b| a < b }.length
 puts result
