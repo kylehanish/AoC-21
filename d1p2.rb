a212a066b65b9aed17d0a2c105bf25787b674bfe
Kyle Hanish <kyle.hanish@gmail.com>
2021-12-03 19:17:12 -0600

    add part 2 with sliding scale of 3

---
+++ d1p2.rb
@@ -0,0 +1,8 @@
+require './input.rb'
+
+@filename = 'd1-input.txt'
+
+lines =  Input.get_file_lines(@filename).compact.map(&:to_i)
+sliding_sums = lines.each_cons(3).map(&:sum)
+result = sliding_sums.each_cons(2).select { |a,b| a < b }.length
+puts result
